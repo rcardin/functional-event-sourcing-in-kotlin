339a1a89d0115344b8c0fb47c7f48a61a79e5344
rcardin <riccardo.cardin@gmail.com>
2023-11-23 09:23:13 +0100

    Added Ktor dependency to the game

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,10 +7,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
  * For more details take a look at the 'Building Java & JVM projects' chapter in the Gradle
  * User Manual available at https://docs.gradle.org/8.1.1/userguide/building_java_projects.html
  */
-
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.9.20"
+    id("io.ktor.plugin") version "2.3.6"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -25,7 +26,13 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
     testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.4.0")
     testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
     implementation("io.arrow-kt:arrow-core:1.2.1")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -38,6 +45,9 @@ java {
 application {
     // Define the main class for the application.
     mainClass.set("in.rcard.fes.AppKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 tasks.named<Test>("test") {
