bff208d921f435d21ce463320465368fc4aa4634
rcardin <riccardo.cardin@gmail.com>
2023-11-28 14:07:44 +0100

    WIP Creating a event store client using arrow-fx resources

--- app/src/main/kotlin/in/rcard/fes/env/Dependencies.kt
+++ app/src/main/kotlin/in/rcard/fes/env/Dependencies.kt
@@ -2,14 +2,22 @@ package `in`.rcard.fes.env
 
 import arrow.fx.coroutines.ResourceScope
 import com.eventstore.dbclient.EventStoreDBClient
+import com.eventstore.dbclient.EventStoreDBConnectionString
+import `in`.rcard.fes.portfolio.PortfolioEventStore
+import `in`.rcard.fes.portfolio.portfolioEventStore
 
-class Dependencies
+class Dependencies(val portfolioEventStore: PortfolioEventStore)
 
 suspend fun ResourceScope.dependencies(env: Env): Dependencies {
     val eventStoreClient = eventStoreClient(env.eventStoreDataSource)
-    return Dependencies()
+    val portfolioEventStore = portfolioEventStore(eventStoreClient)
+    return Dependencies(portfolioEventStore)
 }
 
-suspend fun ResourceScope.eventStoreClient(eventStoreDataSource: Env.EventStoreDataSource): EventStoreDBClient {
-    TODO("Not yet implemented")
-}
+suspend fun ResourceScope.eventStoreClient(eventStoreDataSource: Env.EventStoreDataSource): EventStoreDBClient =
+    install({
+        val settings = EventStoreDBConnectionString.parseOrThrow(eventStoreDataSource.url)
+        EventStoreDBClient.create(settings)
+    }) { client, _ ->
+        client.shutdown()
+    }
--- app/src/main/kotlin/in/rcard/fes/portfolio/eventstore.kt
+++ app/src/main/kotlin/in/rcard/fes/portfolio/eventstore.kt
@@ -1,6 +1,8 @@
 package `in`.rcard.fes.portfolio
 
 import arrow.core.Either
+import com.eventstore.dbclient.EventStoreDBClient
+import `in`.rcard.fes.portfolio.PortfolioEventStore.EventStoreError
 
 typealias ETag = String
 typealias LoadedPortfolio = Pair<ETag, Portfolio>
@@ -15,5 +17,19 @@ interface PortfolioEventStore {
         data class StateLoadingError(val portfolioId: PortfolioId) : EventStoreError
         data class ConcurrentModificationError(val portfolioId: PortfolioId) : EventStoreError
         data class StateSavingError(val portfolioId: PortfolioId) : EventStoreError
+    }
+}
+
+fun portfolioEventStore(eventStoreClient: EventStoreDBClient): PortfolioEventStore = object : PortfolioEventStore {
+    override suspend fun loadState(portfolioId: PortfolioId): Either<EventStoreError, LoadedPortfolio> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveState(
+        portfolioId: PortfolioId,
+        eTag: ETag,
+        portfolio: Portfolio,
+    ): Either<EventStoreError, Unit> {
+        TODO("Not yet implemented")
     }
 }
