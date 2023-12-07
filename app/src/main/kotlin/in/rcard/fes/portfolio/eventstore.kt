8af0eb70c6d0b15cf18cfb2963d574cc553df487
rcardin <riccardo.cardin@gmail.com>
2023-12-07 10:15:11 +0100

    Added test for ConcurrentModificationError in event store writing

--- app/src/main/kotlin/in/rcard/fes/portfolio/eventstore.kt
+++ app/src/main/kotlin/in/rcard/fes/portfolio/eventstore.kt
@@ -30,10 +30,8 @@ typealias LoadedPortfolio = Pair<ETag, Portfolio>
 
 interface PortfolioEventStore {
 
-    context (Json)
     suspend fun loadState(portfolioId: PortfolioId): Either<EventStoreError, LoadedPortfolio>
 
-    context (Json)
     suspend fun saveState(
         portfolioId: PortfolioId,
         eTag: ETag,
@@ -49,71 +47,72 @@ interface PortfolioEventStore {
 }
 
 context (Json)
-fun portfolioEventStore(eventStoreClient: EventStoreDBClient): PortfolioEventStore = object : PortfolioEventStore {
+fun portfolioEventStore(eventStoreClient: EventStoreDBClient): PortfolioEventStore =
+    object : PortfolioEventStore {
 
-    override suspend fun loadState(portfolioId: PortfolioId): Either<EventStoreError, LoadedPortfolio> {
-        val options = ReadStreamOptions.get()
-            .forwards()
-            .fromStart()
+        override suspend fun loadState(portfolioId: PortfolioId): Either<EventStoreError, LoadedPortfolio> {
+            val options = ReadStreamOptions.get()
+                .forwards()
+                .fromStart()
 
-        // TODO Check if the stream exists
-        val result: ReadResult = eventStoreClient.readStream("portfolio-${portfolioId.id}", options).await()
+            // TODO Check if the stream exists
+            val result: ReadResult = eventStoreClient.readStream("portfolio-${portfolioId.id}", options).await()
 
-        val eTag: Long = maxPosition(result.events)
-        val loadedEvents =
-            result.events.map { decodeFromString<PortfolioEvent>(it.originalEvent.eventData.decodeToString()) }
-        return (eTag to loadedEvents).right()
-    }
-
-    private fun maxPosition(events: List<ResolvedEvent>) =
-        events.maxByOrNull { it.originalEvent.revision }!!.originalEvent.revision
-
-    override suspend fun saveState(
-        portfolioId: PortfolioId,
-        eTag: ETag,
-        oldPortfolio: Portfolio,
-        newPortfolio: Portfolio,
-    ): Either<EventStoreError, PortfolioId> = either {
-        val eventsToPersist = newPortfolio - oldPortfolio
-
-        val appendToStreamOptions: AppendToStreamOptions = AppendToStreamOptions.get().let { options ->
-            when (eTag) {
-                -1L -> options.expectedRevision(noStream())
-                else -> options.expectedRevision(ExpectedRevision.expectedRevision(eTag))
-            }
+            val eTag: Long = maxPosition(result.events)
+            val loadedEvents =
+                result.events.map { decodeFromString<PortfolioEvent>(it.originalEvent.eventData.decodeToString()) }
+            return (eTag to loadedEvents).right()
         }
 
-        val eventDataList = eventsToPersist.map { event ->
-            EventDataBuilder.json(
-                UUID.randomUUID(),
-                event.eventType(),
-                encodeToString(event).encodeToByteArray(),
-            ).build()
-        }
+        private fun maxPosition(events: List<ResolvedEvent>) =
+            events.maxByOrNull { it.originalEvent.revision }!!.originalEvent.revision
 
-        catch(
-            {
-                eventStoreClient.appendToStream(
-                    "portfolio-${portfolioId.id}",
-                    appendToStreamOptions,
-                    eventDataList.iterator(),
-                )
-                    .await()
-            },
-        ) { error: Throwable ->
-            when (error) {
-                is WrongExpectedVersionException -> raise(ConcurrentModificationError(portfolioId))
-                // TODO Is it correct?
-                is CancellationException -> throw error
-                else -> {
-                    println("The error is: $error")
-                    raise(EventStoreError.StateSavingError(portfolioId))
+        override suspend fun saveState(
+            portfolioId: PortfolioId,
+            eTag: ETag,
+            oldPortfolio: Portfolio,
+            newPortfolio: Portfolio,
+        ): Either<EventStoreError, PortfolioId> = either {
+            val eventsToPersist = newPortfolio - oldPortfolio
+
+            val appendToStreamOptions: AppendToStreamOptions = AppendToStreamOptions.get().let { options ->
+                when (eTag) {
+                    -1L -> options.expectedRevision(noStream())
+                    else -> options.expectedRevision(ExpectedRevision.expectedRevision(eTag))
                 }
             }
+
+            val eventDataList = eventsToPersist.map { event ->
+                EventDataBuilder.json(
+                    UUID.randomUUID(),
+                    event.eventType(),
+                    encodeToString(event).encodeToByteArray(),
+                ).build()
+            }
+
+            catch(
+                {
+                    eventStoreClient.appendToStream(
+                        "portfolio-${portfolioId.id}",
+                        appendToStreamOptions,
+                        eventDataList.iterator(),
+                    )
+                        .await()
+                },
+            ) { error: Throwable ->
+                when (error) {
+                    is WrongExpectedVersionException -> raise(ConcurrentModificationError(portfolioId))
+                    // TODO Is it correct?
+                    is CancellationException -> throw error
+                    else -> {
+                        println("The error is: $error")
+                        raise(EventStoreError.StateSavingError(portfolioId))
+                    }
+                }
+            }
+            portfolioId
         }
-        portfolioId
     }
-}
 
 private fun PortfolioEvent.eventType(): String = when (this) {
     is PortfolioCreated -> "PortfolioCreated"
--- app/src/test/kotlin/in/rcard/fes/EventStoreTest.kt
+++ app/src/test/kotlin/in/rcard/fes/EventStoreTest.kt
@@ -7,10 +7,12 @@ import `in`.rcard.eventstore.DockerContainerDatabase
 import `in`.rcard.fes.portfolio.Money
 import `in`.rcard.fes.portfolio.PortfolioEvent
 import `in`.rcard.fes.portfolio.PortfolioEvent.PortfolioCreated
+import `in`.rcard.fes.portfolio.PortfolioEventStore.EventStoreError.ConcurrentModificationError
 import `in`.rcard.fes.portfolio.PortfolioId
 import `in`.rcard.fes.portfolio.UserId
 import `in`.rcard.fes.portfolio.notCreatedPortfolio
 import `in`.rcard.fes.portfolio.portfolioEventStore
+import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.core.extensions.install
 import io.kotest.core.spec.style.ShouldSpec
@@ -23,7 +25,7 @@ private val NOW_MILLIS = Instant.now().toEpochMilli()
 
 class EventStoreTest : ShouldSpec({
     context("The event store module") {
-        should("be able to connect to the event store") {
+        should("write event stream to the event store") {
             val eventStoreDb = install(
                 ContainerExtension(
                     DockerContainerDatabase(
@@ -61,6 +63,48 @@ class EventStoreTest : ShouldSpec({
                             Money(100.0),
                         ),
                     )
+            }
+        }
+
+        should("return a ConcurrentModificationError in case of concurrent modification of the stream") {
+            val eventStoreDb = install(
+                ContainerExtension(
+                    DockerContainerDatabase(
+                        DockerContainerDatabase.Builder().version("latest")
+                            .secure(false),
+                    ),
+                ),
+            )
+            val eventStoreDBClient = eventStoreDb.defaultClient()
+            with(Json) {
+                val portfolioEventStore = portfolioEventStore(eventStoreDBClient)
+                portfolioEventStore.saveState(
+                    PortfolioId("1"),
+                    -1L,
+                    notCreatedPortfolio,
+                    nonEmptyListOf(
+                        PortfolioCreated(
+                            PortfolioId("1"),
+                            NOW_MILLIS,
+                            UserId("rcardin"),
+                            Money(100.0),
+                        ),
+                    ),
+                ).shouldBeRight(PortfolioId("1"))
+
+                portfolioEventStore.saveState(
+                    PortfolioId("1"),
+                    -1L,
+                    notCreatedPortfolio,
+                    nonEmptyListOf(
+                        PortfolioCreated(
+                            PortfolioId("1"),
+                            NOW_MILLIS,
+                            UserId("rcardin"),
+                            Money(100.0),
+                        ),
+                    ),
+                ).shouldBeLeft(ConcurrentModificationError(PortfolioId("1")))
             }
         }
     }
