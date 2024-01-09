54da6a5d39a7a020e421fe499a020b74e4644b3d
rcardin <riccardo.cardin@gmail.com>
2024-01-09 09:29:44 +0100

    First implementation of the FindStockPriceBySymbol interface

--- app/src/main/kotlin/in/rcard/fes/stock/ports.kt
+++ app/src/main/kotlin/in/rcard/fes/stock/ports.kt
@@ -1,13 +1,30 @@
 package `in`.rcard.fes.stock
 
 import arrow.core.Either
+import arrow.core.raise.catch
+import arrow.core.raise.either
 import `in`.rcard.fes.portfolio.Money
 import `in`.rcard.fes.portfolio.Stock
+import `in`.rcard.fes.sqldelight.StockPricesQueries
+import `in`.rcard.fes.stock.StockPricesError.StockPricesGenericError
 
-sealed interface StockError {
-    // TODO Add possible errors
+sealed interface StockPricesError {
+    data object StockPricesGenericError : StockPricesError
 }
 
-interface FindStockBySymbol {
-    suspend fun findPriceBySymbol(symbol: Stock): Either<StockError, Money?>
+interface FindStockPriceBySymbol {
+    // TODO Is it correct to return a StockPricesError?
+    suspend fun findPriceBySymbol(symbol: Stock): Either<StockPricesError, Money?>
 }
+
+fun stockPricesRepository(stockPricesQueries: StockPricesQueries): FindStockPriceBySymbol =
+    object : FindStockPriceBySymbol {
+        override suspend fun findPriceBySymbol(symbol: Stock): Either<StockPricesError, Money?> =
+            either {
+                catch({
+                    stockPricesQueries.findPriceByStockId(symbol).executeAsOneOrNull()?.price
+                }) {
+                    raise(StockPricesGenericError)
+                }
+            }
+    }
