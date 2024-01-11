d84a10193449d00d389b0c3115a5a1580406cd81
rcardin <riccardo.cardin@gmail.com>
2024-01-11 09:18:57 +0100

    Added test for the stockPricesRepository

--- app/src/test/kotlin/in/rcard/fes/stock/FindStockPriceBySymbolTest.kt
+++ app/src/test/kotlin/in/rcard/fes/stock/FindStockPriceBySymbolTest.kt
@@ -2,6 +2,11 @@ package `in`.rcard.fes.stock
 
 import arrow.fx.coroutines.continuations.resource
 import `in`.rcard.fes.env.sqlDelight
+import `in`.rcard.fes.portfolio.Money
+import `in`.rcard.fes.portfolio.Stock
+import `in`.rcard.fes.sqldelight.StockPrices
+import io.kotest.assertions.arrow.core.shouldBeLeft
+import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.assertions.arrow.fx.coroutines.extension
 import io.kotest.core.extensions.install
 import io.kotest.core.spec.style.ShouldSpec
@@ -20,9 +25,21 @@ class FindStockPriceBySymbolTest : ShouldSpec({
             }.extension(),
         )
 
+    beforeSpec {
+        database.get().stockPricesQueries.insertStockPrice(StockPrices(Stock("AAPL"), Money(100.0)))
+    }
+
     context("The findPriceBySymbol function") {
+
         should("return the price of the stock") {
             val stockPricesRepository = stockPricesRepository(database.get().stockPricesQueries)
+            stockPricesRepository.findPriceBySymbol(Stock("AAPL")).shouldBeRight(Money(100.0))
+        }
+
+        should("return a StockPricesNotFoundError if the symbol is not present") {
+            val stockPricesRepository = stockPricesRepository(database.get().stockPricesQueries)
+            stockPricesRepository.findPriceBySymbol(Stock("GOOG"))
+                .shouldBeLeft(StockPricesError.StockPricesNotFoundError(Stock("GOOG")))
         }
     }
 })
