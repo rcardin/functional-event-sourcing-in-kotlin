1134beecbcea6a3ff9407fe2bcd94324fb70b319
rcardin <riccardo.cardin@gmail.com>
2024-01-19 16:00:48 +0100

    Moved the validation rules inside the type classes

--- app/src/main/kotlin/in/rcard/fes/portfolio/validation.kt
+++ app/src/main/kotlin/in/rcard/fes/portfolio/validation.kt
@@ -6,8 +6,8 @@ import arrow.core.EitherNel
 import arrow.core.left
 import arrow.core.right
 import arrow.core.toEitherNel
-import `in`.rcard.fes.portfolio.InvalidFieldError.NegativeFieldError
 import `in`.rcard.fes.portfolio.InvalidFieldError.MissingFieldError
+import `in`.rcard.fes.portfolio.InvalidFieldError.NegativeFieldError
 import `in`.rcard.fes.portfolio.InvalidFieldError.ZeroFieldError
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.request.receive
@@ -17,7 +17,6 @@ import kotlinx.serialization.Serializable
 data class GenericErrorDTO(val errors: List<String>)
 
 sealed interface InvalidFieldError {
-
     val field: String
 
     data class MissingFieldError(override val field: String) : InvalidFieldError {
@@ -33,88 +32,87 @@ sealed interface InvalidFieldError {
     }
 }
 
-fun ValidationError.toGenericError(): GenericErrorDTO =
-    GenericErrorDTO(fieldErrors.all.map { it.toString() })
+fun ValidationError.toGenericError(): GenericErrorDTO = GenericErrorDTO(fieldErrors.all.map { it.toString() })
 
 interface ValidationScope<T> {
     fun T.validate(): Either<ValidationError, T>
 }
 
 context (ValidationScope<T>)
-suspend inline fun <reified T : Any> ApplicationCall.validate(): Either<ValidationError, T> =
-    receive<T>().validate()
+suspend inline fun <reified T : Any> ApplicationCall.validate(): Either<ValidationError, T> = receive<T>().validate()
 
 interface Required<T> {
     fun T.required(): Boolean
+
+    fun T.required(fieldName: String): EitherNel<MissingFieldError, T> =
+        if (required()) {
+            this.right()
+        } else {
+            MissingFieldError(fieldName).left().toEitherNel()
+        }
 }
 
 interface Positive<T : Number> {
     fun T.positive(): Boolean
+
+    fun T.positive(fieldName: String): EitherNel<NegativeFieldError, T> =
+        if (positive()) {
+            this.right()
+        } else {
+            NegativeFieldError(fieldName).left().toEitherNel()
+        }
 }
 
 interface NonZero<T : Number> {
     fun T.nonZero(): Boolean
-}
 
-val requiredString = object : Required<String> {
-    override fun String.required(): Boolean = this.isNotBlank()
-}
-
-val positiveDouble = object : Positive<Double> {
-    override fun Double.positive(): Boolean = this > 0.0
-}
-
-val nonZeroInteger = object : NonZero<Int> {
-    override fun Int.nonZero(): Boolean = this != 0
-}
-
-context(Required<T>)
-fun <T> T.required(fieldName: String): EitherNel<MissingFieldError, T> =
-    if (required()) {
-        this.right()
-    } else {
-        MissingFieldError(fieldName).left().toEitherNel()
-    }
-
-context(Positive<T>)
-fun <T : Number> T.positive(fieldName: String): EitherNel<NegativeFieldError, T> =
-    if (this.positive()) {
-        this.right()
-    } else {
-        NegativeFieldError(fieldName).left().toEitherNel()
-    }
-
-context(NonZero<T>)
-fun <T : Number> T.nonZero(fieldName: String): EitherNel<ZeroFieldError, T> =
-    if (this.nonZero()) {
-        this.right()
-    } else {
-        ZeroFieldError(fieldName).left().toEitherNel()
-    }
-
-val createPortfolioDTOValidator = object : ValidationScope<CreatePortfolioDTO> {
-    override fun CreatePortfolioDTO.validate(): Either<ValidationError, CreatePortfolioDTO> =
-        with(requiredString) {
-            with(positiveDouble) {
-                zipOrAccumulate(
-                    userId.required("userId"),
-                    amount.positive("amount"),
-                    ::CreatePortfolioDTO,
-                ).mapLeft(::ValidationError)
-            }
+    fun T.nonZero(fieldName: String): EitherNel<ZeroFieldError, T> =
+        if (nonZero()) {
+            this.right()
+        } else {
+            ZeroFieldError(fieldName).left().toEitherNel()
         }
 }
 
-val changePortfolioDTOValidator = object : ValidationScope<ChangePortfolioDTO> {
-    override fun ChangePortfolioDTO.validate(): Either<ValidationError, ChangePortfolioDTO> =
-        with(requiredString) {
-            with(nonZeroInteger) {
-                zipOrAccumulate(
-                    stock.required("stock"),
-                    quantity.nonZero("quantity"),
-                    ::ChangePortfolioDTO,
-                ).mapLeft(::ValidationError)
-            }
-        }
-}
+val requiredString =
+    object : Required<String> {
+        override fun String.required(): Boolean = this.isNotBlank()
+    }
 
+val positiveDouble =
+    object : Positive<Double> {
+        override fun Double.positive(): Boolean = this > 0.0
+    }
+
+val nonZeroInteger =
+    object : NonZero<Int> {
+        override fun Int.nonZero(): Boolean = this != 0
+    }
+
+val createPortfolioDTOValidator =
+    object : ValidationScope<CreatePortfolioDTO> {
+        override fun CreatePortfolioDTO.validate(): Either<ValidationError, CreatePortfolioDTO> =
+            with(requiredString) {
+                with(positiveDouble) {
+                    zipOrAccumulate(
+                        userId.required("userId"),
+                        amount.positive("amount"),
+                        ::CreatePortfolioDTO,
+                    ).mapLeft(::ValidationError)
+                }
+            }
+    }
+
+val changePortfolioDTOValidator =
+    object : ValidationScope<ChangePortfolioDTO> {
+        override fun ChangePortfolioDTO.validate(): Either<ValidationError, ChangePortfolioDTO> =
+            with(requiredString) {
+                with(nonZeroInteger) {
+                    zipOrAccumulate(
+                        stock.required("stock"),
+                        quantity.nonZero("quantity"),
+                        ::ChangePortfolioDTO,
+                    ).mapLeft(::ValidationError)
+                }
+            }
+    }
