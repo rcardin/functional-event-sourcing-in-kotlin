db3b4e934600ee4ba3018896ba84b4f98a57b5e5
rcardin <riccardo.cardin@gmail.com>
2024-02-21 13:58:16 +0100

    [WIP] Trying to model listeners as Flows

--- app/src/main/kotlin/in/rcard/fes/projection/listeners.kt
+++ app/src/main/kotlin/in/rcard/fes/projection/listeners.kt
@@ -2,47 +2,54 @@ package `in`.rcard.fes.projection
 
 import com.eventstore.dbclient.EventStoreDBClient
 import com.eventstore.dbclient.ResolvedEvent
+import com.eventstore.dbclient.SubscribeToAllOptions
 import com.eventstore.dbclient.Subscription
+import com.eventstore.dbclient.SubscriptionFilter
 import com.eventstore.dbclient.SubscriptionListener
 import `in`.rcard.fes.portfolio.PortfolioEvent
+import `in`.rcard.fes.portfolio.PortfolioEvent.PortfolioCreated
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.serialization.json.Json.Default.decodeFromString
 import org.slf4j.Logger
 
-interface PortfolioEventListener {
-    suspend fun start()
-}
-
 context(Logger)
-fun portfolioEventListener(
-    eventStoreClient: EventStoreDBClient,
-    createPortfolioUseCase: CreatePortfolioUseCase,
-): PortfolioEventListener =
-    object : PortfolioEventListener {
-        val logger = this@Logger
-
-        override suspend fun start()  {
-            val sl: SubscriptionListener =
-                object : SubscriptionListener() {
-                    override fun onEvent(
-                        subscription: Subscription?,
-                        event: ResolvedEvent?,
-                    ) {
-                        if (event != null) {
-                            logger.info(
-                                "Event received: ${event.event.eventType} for stream ${event.event.streamId} " +
-                                    "by subscription: ${subscription?.subscriptionId}",
-                            )
-                            val portfolioEvent =
-                                decodeFromString<PortfolioEvent>(event.originalEvent.eventData.decodeToString())
-                            when (portfolioEvent) {
-                                is PortfolioEvent.PortfolioCreated -> createPortfolioUseCase.createPortfolio(portfolioEvent)
-                                is PortfolioEvent.StocksPurchased -> logger.info("Stocks purchased: $portfolioEvent")
-                                is PortfolioEvent.StocksSold -> logger.info("Stocks sold: $portfolioEvent")
-                                is PortfolioEvent.PortfolioClosed -> logger.info("Portfolio closed: $portfolioEvent")
-                            }
-                        }
+fun portfolioCreatedEventFlow(eventStoreClient: EventStoreDBClient): Flow<PortfolioEvent> =
+    channelFlow {
+        val subscriptionListener =
+            object : SubscriptionListener() {
+                override fun onEvent(
+                    subscription: Subscription?,
+                    event: ResolvedEvent?,
+                ) {
+                    if (event != null) {
+                        this@Logger.info(
+                            "Event received: ${event.event.eventType} for stream ${event.event.streamId} " +
+                                "by subscription: ${subscription?.subscriptionId}",
+                        )
+                        val portfolioEvent =
+                            decodeFromString<PortfolioEvent>(event.originalEvent.eventData.decodeToString()) as PortfolioCreated
+                        trySendBlocking(portfolioEvent)
                     }
                 }
-            eventStoreClient.subscribeToAll(sl)
-        }
+
+                override fun onCancelled(
+                    subscription: Subscription?,
+                    exception: Throwable?,
+                ) {
+                    cancel("Subscription to portfolio-created stream was cancelled", exception)
+                }
+            }
+        val subscriptionFilter =
+            SubscribeToAllOptions.get()
+                .filter(
+                    SubscriptionFilter.newBuilder()
+                        .withEventTypeRegularExpression("portfolio-create")
+                        .build(),
+                )
+        eventStoreClient.subscribeToAll(subscriptionListener, subscriptionFilter)
+        awaitClose()
     }
